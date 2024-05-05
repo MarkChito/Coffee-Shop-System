74b464be5455afcda647c04812219253c58a7d83
CarloPhillipAnteja <carlophillipanteja19@gmail.com>
2024-05-05 19:55:15 +0800

    footer-updated

--- views/pages/home_view.php
+++ views/pages/home_view.php
@@ -6,14 +6,14 @@
         </div>
 
         <div class="image">
-            <img src="assets/image/home-img-1.png" class="main-home-image" alt="">
+            <img src="<?= $base_url ?>assets/image/home-img-1.png" class="main-home-image" alt="">
         </div>
     </div>
 
     <div class="image-slider">
-        <img src="assets/image/home-img-1.png" alt="">
-        <img src="assets/image/home-img-2.png" alt="">
-        <img src="assets/image/home-img-3.png" alt="">
+        <img src="<?= $base_url ?>assets/image/home-img-1.png" alt="">
+        <img src="<?= $base_url ?>assets/image/home-img-2.png" alt="">
+        <img src="<?= $base_url ?>assets/image/home-img-3.png" alt="">
     </div>
 </section>
 
@@ -23,7 +23,7 @@
 
     <div class="row">
         <div class="image">
-            <img src="assets/image/about-img.png" alt="">
+            <img src="<?= $base_url ?>assets/image/about-img.png" alt="">
         </div>
 
         <div class="content">
@@ -40,15 +40,15 @@
             <a href="#" class="btn">read more</a>
             <div class="icons-container">
                 <div class="icons">
-                    <img src="assets/image/about-icon-1.png" alt="">
+                    <img src="<?= $base_url ?>assets/image/about-icon-1.png" alt="">
                     <h3>quality coffee</h3>
                 </div>
                 <div class="icons">
-                    <img src="assets/image/about-icon-2.png" alt="">
+                    <img src="<?= $base_url ?>assets/image/about-icon-2.png" alt="">
                     <h3>our branches</h3>
                 </div>
                 <div class="icons">
-                    <img src="assets/image/about-icon-3.png" alt="">
+                    <img src="<?= $base_url ?>assets/image/about-icon-3.png" alt="">
                     <h3>free delivery</h3>
                 </div>
             </div>
@@ -64,56 +64,56 @@
     <div class="shop-content">
         <!-- BOX 1 -->
         <div class="product-box">
-            <img src="assets/image/coffee1-Cafecito-de-Olla.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee1-Cafecito-de-Olla.png" alt="" class="product-img">
             <h2 class="product-title">Cafecito de Olla</h2>
             <span class="product-price">₱239</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 2 -->
         <div class="product-box">
-            <img src="assets/image/coffee2-Cinnamon-Coffee-Smoothie.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee2-Cinnamon-Coffee-Smoothie.png" alt="" class="product-img">
             <h2 class="product-title">Cinnamon Coffee Smoothie</h2>
             <span class="product-price">₱269</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 3 -->
         <div class="product-box">
-            <img src="assets/image/coffee3-Creamy-Gourmet-Hot-Chocolate.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee3-Creamy-Gourmet-Hot-Chocolate.png" alt="" class="product-img">
             <h2 class="product-title">Creamy Gourmet Hot Chocolate</h2>
             <span class="product-price">₱219</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 4 -->
         <div class="product-box">
-            <img src="assets/image/coffee7-Iced-White-Mocha-with-Caramel-and-Salted-Cold-Foam.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee7-Iced-White-Mocha-with-Caramel-and-Salted-Cold-Foam.png" alt="" class="product-img">
             <h2 class="product-title">Iced White Mocha with Caramel</h2>
             <span class="product-price">₱299</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 5 -->
         <div class="product-box">
-            <img src="assets/image/coffee5-Frozen-Irish-Coffee.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee5-Frozen-Irish-Coffee.png" alt="" class="product-img">
             <h2 class="product-title">Frozen Irish Coffee</h2>
             <span class="product-price">₱309</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 6 -->
         <div class="product-box">
-            <img src="assets/image/coffee6-Honey-Almond-Milk-Flat-White.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee6-Honey-Almond-Milk-Flat-White.png" alt="" class="product-img">
             <h2 class="product-title">Honey Almond Milk Flat White</h2>
             <span class="product-price">₱249</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 7 -->
         <div class="product-box">
-            <img src="assets/image/coffee8-Maple-Bacon-Latte.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee8-Maple-Bacon-Latte.png" alt="" class="product-img">
             <h2 class="product-title">Maple Bacon Latte</h2>
             <span class="product-price">₱239</span>
             <i class='fas fa-shopping-cart add-cart'></i>
         </div>
         <!-- BOX 8 -->
         <div class="product-box">
-            <img src="assets/image/coffee9-Pumpkin-Spice-Coffee.png" alt="" class="product-img">
+            <img src="<?= $base_url ?>assets/image/coffee9-Pumpkin-Spice-Coffee.png" alt="" class="product-img">
             <h2 class="product-title">Pumpkin-Spice-Coffee</h2>
             <span class="product-price">₱319</span>
             <i class='fas fa-shopping-cart add-cart'></i>
@@ -130,7 +130,7 @@
             <div class="swiper-slide box">
                 <i class="fas fa-quote-left"></i>
                 <i class="fas fa-quote-right"></i>
-                <img src="assets/image/pic-1.png" alt="">
+                <img src="<?= $base_url ?>assets/image/pic-1.png" alt="">
                 <div class="stars">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -147,7 +147,7 @@
             <div class="swiper-slide box">
                 <i class="fas fa-quote-left"></i>
                 <i class="fas fa-quote-right"></i>
-                <img src="assets/image/pic-2.png" alt="">
+                <img src="<?= $base_url ?>assets/image/pic-2.png" alt="">
                 <div class="stars">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -164,7 +164,7 @@
             <div class="swiper-slide box">
                 <i class="fas fa-quote-left"></i>
                 <i class="fas fa-quote-right"></i>
-                <img src="assets/image/pic-3.png" alt="">
+                <img src="<?= $base_url ?>assets/image/pic-3.png" alt="">
                 <div class="stars">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -181,7 +181,7 @@
             <div class="swiper-slide box">
                 <i class="fas fa-quote-left"></i>
                 <i class="fas fa-quote-right"></i>
-                <img src="assets/image/pic-4.png" alt="">
+                <img src="<?= $base_url ?>assets/image/pic-4.png" alt="">
                 <div class="stars">
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,17 +1,17 @@
 <section class="footer">
     <div class="box-container">
         <div class="box">
-            <h3>quick links</h3>
-            <a href="#home" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> home</a>
-            <a href="#about" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> about</a>
-            <a href="#menu" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> menu</a>
-            <a href="#review" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> review</a>
-            <a href="#book" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> book</a>
-            <a href="<?= $base_url ?>login" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> account</a>
+            <h3>Quick Links</h3>
+            <a href="#home" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> Home</a>
+            <a href="#about" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> About</a>
+            <a href="#menu" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> Menu</a>
+            <a href="#review" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> Review</a>
+            <a href="#book" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> Book</a>
+            <a href="<?= $base_url ?>login" style="text-decoration: none;"><i class="fas fa-arrow-right"></i> Account</a>
         </div>
 
         <div class="box">
-            <h3>contact info</h3>
+            <h3>Contact Info</h3>
             <a href="javascript:void(0)" style="text-decoration: none;"><i class="fas fa-phone"></i> +123-456-7890</a>
             <a href="javascript:void(0)" style="text-decoration: none;"><i class="fas fa-phone"></i> +111-222-3333</a>
             <a href="javascript:void(0)" style="text-decoration: none;"><i class="fas fa-envelope"></i> coffeeshop@gmail.com</a>
@@ -19,17 +19,17 @@
         </div>
 
         <div class="box">
-            <h3>contact info</h3>
-            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-facebook-f"></i> facebook</a>
-            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-twitter"></i> twitter</a>
-            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-instagram"></i> instagram</a>
-            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-linkedin"></i> linkedin</a>
-            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-twitter"></i> twitter</a>
+            <h3>Contact Info</h3>
+            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-facebook-f"></i> Facebook</a>
+            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-instagram"></i> Instagram</a>
+            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-linkedin"></i> Linkedin</a>
+            <a href="javascript:void(0)" style="text-decoration: none;"><i class="fab fa-twitter"></i> Twitter</a>
         </div>
     </div>
 
     <div class="credit">
-        <span>&copy; All rights reserved</span>
+        <span>&copy; All Rights Reserved</span>
     </div>
 </section>
 
